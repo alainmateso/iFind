39b46511d1afd7273868c863d2489708aca5b24c
Macheza Dzabala <dzabalamacheza@gmail.com>
2019-09-18 10:47:17 +0200

    - Used static classes for userController
    - Codeclimate coverage reporting
    
    - Removed Signin Controller
    
    - Removed Signup Controller
    
    [#168532824, #168532778]

---
+++ controllers/authentication/userController.js
@@ -0,0 +1,40 @@
+import models from '../../models';
+import responseHelper from '../../helpers/responseHelper';
+import strings from '../../helpers/stringsHelper';
+import { hashPassword, compareHash } from '../../helpers/hashHelper';
+import { signUserToken } from '../../helpers/tokenHelper';
+
+
+export default class userController {
+  static async signupUser({ body }, res) {
+    const user = models.users.build({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      phonenumber: body.phonenumber,
+      email: body.email,
+      password: hashPassword(body.password),
+    });
+    user.save().then(() => {
+      const token = signUserToken(user);
+      return responseHelper(res, 201, strings.users.successMessages.SUCCESSFULLY_CREATED_USER, { token });
+    });
+  }
+
+
+  static async signinUser({ body }, res) {
+    models.users.findOne({
+      where: {
+        email: body.email,
+      },
+    }).then((user) => {
+      if (user === null) {
+        return responseHelper(res, 404, strings.users.errorMessages.USER_NOT_FOUND_SIGNIN_REQUEST);
+      }
+      if (compareHash(user.dataValues.password, body.password)) {
+        const token = signUserToken(user);
+        return responseHelper(res, 201, strings.users.successMessages.SUCCESSFULLY_SIGNED_IN_USER, { token });
+      }
+      return responseHelper(res, 404, strings.users.errorMessages.USER_NOT_FOUND_SIGNIN_REQUEST);
+    });
+  }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { checkSignUp, verifyExistence, checkSignIn } from '../middlewares/authMiddleware';
-import { signupUser } from '../controllers/authentication/signupController';
-import { signinUser } from '../controllers/authentication/signinController';
+import userController from '../controllers/authentication/userController';
 
 const router = new Router();
 
-
-router.post('/signup', checkSignUp, verifyExistence, (req, res) => signupUser(req, res));
-router.post('/signin', checkSignIn, (req, res) => signinUser(req, res));
+router.post('/signup', checkSignUp, verifyExistence, (req, res) => userController.signupUser(req, res));
+router.post('/signin', checkSignIn, (req, res) => userController.signinUser(req, res));
 
 export default router;

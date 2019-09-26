f56efe32a0b698ae0fb68b4669f11b952ca72197
Macheza Dzabala <dzabalamacheza@gmail.com>
2019-09-26 09:52:10 +0200

    CH add default seeders / Apply feedback
    - Removed all commented code
    - Added an image migration on Posts table
    - Added image field to posts model
    
    [#168752605]

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,15 +1,13 @@
 import signUpTests from './__auth__/signupTest.spec';
 import signInTests from './__auth__/signinTest.spec';
-import getPostsTests from './__posts__/getPostsTest.spec';
 import getOnePostTests from './__posts__/getOnePostTest.spec';
 import postTests from './__posts__/postTests';
-import adminTest from'./__admin__/adminTest.spec';
+import adminTest from './__admin__/adminTest.spec';
 import deletePost from './__posts__/deletePost.spec';
 
 describe('Test Runner', () => {
   describe('Signup Tests', signUpTests);
   describe('Signin Tests', signInTests);
-  describe('Get Posts Tests', getPostsTests);
   describe('Get One Post Tests', getOnePostTests);
   describe('Create New Post Test', postTests);
   describe('Create New category', adminTest);

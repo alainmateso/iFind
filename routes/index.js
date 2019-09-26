aa93441d6c8eb6126d720cd41529091e0a4f427f
Thomas Nignan <thomasnignan@Thomass-MacBook-Pro.local>
2019-09-26 16:15:52 +0200

    chore(documentation): iFind Swagger Documentation
    - install swagger-ui-express to the dependencies
    
    - create /api-docs routes
    
    - create swagger.json file and define iFind API documentation
    
    [Finishes #168784075]

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import authRoutes from './auth';
 import postRoutes from './posts';
 import adminRoutes from './admin';
+import swaggerRoutes from './swagger';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/posts', postRoutes);
 router.use('/admin', adminRoutes);
-router.get('*', (req, res) => res.status(404).send({ message: 'This is ^Caret, Route not foun on Lost and Found' }));
+router.use('/api-docs', swaggerRoutes);
+router.get('*', (req, res) => res.status(404).send({ message: 'This is ^Caret, Route not found on Lost and Found' }));
 
 export default router;
